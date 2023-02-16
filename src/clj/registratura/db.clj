ff45a5c6f88d60e484d12f50b9b76f28060dd7a1
Vsevolod Romashov <7@7vn.ru>
2023-02-16 21:14:40 +0400

    refactor db initialization

--- src/clj/registratura/db.clj
+++ src/clj/registratura/db.clj
@@ -9,15 +9,14 @@
                    [query])
                  jdbc/snake-kebab-opts))
 
-(defn- db-initialized? [db-conn]
-  (let [tables-query "SELECT table_name
-                      FROM information_schema.tables
-                      WHERE table_type = 'BASE TABLE'
-                      AND table_schema = 'public'"
-        table-names (->> (make-query db-conn tables-query)
-                         (map :tables/table-name)
-                         (into #{}))]
-    (contains? table-names "patients")))
+(defn- fresh-db? [db-conn]
+  (let [tables-query ["SELECT 1
+                       FROM information_schema.tables
+                       WHERE table_type = 'BASE TABLE'
+                       AND table_schema = 'public'
+                       AND table_name = ?"
+                      "patients"]]
+    (empty? (make-query db-conn tables-query))))
 
 (defn- get-db-schema []
   (-> "schema.sql"
@@ -26,7 +25,7 @@
 
 (defn start [db-spec]
   (let [conn (jdbc/get-connection db-spec)]
-    (when-not (db-initialized? conn)
+    (when (fresh-db? conn)
       (make-query conn (get-db-schema)))
     conn))
 
