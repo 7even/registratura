c5ad75a02d143a648df989f0d627129f105d20ea
Vsevolod Romashov <7@7vn.ru>
2023-02-23 18:21:02 +0400

    load patients when opening the patients list page

--- src/cljs/registratura/patients_list.cljs
+++ src/cljs/registratura/patients_list.cljs
@@ -1,9 +1,24 @@
 (ns registratura.patients-list
-  (:require [re-frame.core :as rf]))
+  (:require day8.re-frame.http-fx
+            [registratura.http :as http]
+            [re-frame.core :as rf]))
 
 (rf/reg-event-fx ::load-patients
   (fn []
-    (println "loading patients")))
+    {:fx [[:http-xhrio {:method :get
+                        :uri "/api/patients"
+                        :response-format (http/edn-response-format)
+                        :on-success [::patients-loaded]
+                        :on-failure [::failed-to-load-patients]}]]}))
+
+(rf/reg-event-db ::patients-loaded
+  (fn [db [_ patients]]
+    (assoc db :patients patients)))
+
+;; TODO: render unhandled error in the interface
+(rf/reg-event-db ::failed-to-load-patients
+  (fn [db]
+    (assoc db :unhandled-error? true)))
 
 (defn page []
   [:div {:style {:display :flex
