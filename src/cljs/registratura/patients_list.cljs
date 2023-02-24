a26f6fec6c9a39036a1a2514f00ae0699db4ab46
Vsevolod Romashov <7@7vn.ru>
2023-02-24 23:11:05 +0400

    add a search string for patients table

--- src/cljs/registratura/patients_list.cljs
+++ src/cljs/registratura/patients_list.cljs
@@ -1,7 +1,7 @@
 (ns registratura.patients-list
   (:require [clojure.string :as str]
             day8.re-frame.http-fx
-            [registratura.common :refer [<sub]]
+            [registratura.common :refer [<sub >evt]]
             [registratura.http :as http]
             [registratura.routes :as routes]
             [re-frame.core :as rf]
@@ -25,24 +25,51 @@
   (fn [db]
     (assoc db :unhandled-error? true)))
 
+(rf/reg-event-db ::change-patients-filter
+  (fn [db [_ new-filter]]
+    (assoc db :patients-filter new-filter)))
+
 (def ^:private date-formatter
   (t/formatter "dd.MM.YYYY"))
 
 (rf/reg-sub ::patients
   (fn [db]
-    (->> (:patients db)
-         (map (fn [{:patient/keys [first-name
-                                   middle-name
-                                   last-name]
-                    :as patient}]
-                (-> patient
-                    (dissoc :patient/first-name :patient/middle-name :patient/last-name)
-                    (assoc :patient/full-name
-                           (->> [first-name middle-name last-name]
-                                (remove str/blank?)
-                                (str/join " ")))
-                    (update :patient/gender (comp str/capitalize name))
-                    (update :patient/birthday (partial t/format date-formatter))))))))
+    (let [current-filter (:patients-filter db)]
+      (cond->> (:patients db)
+        :always
+        (map (fn [{:patient/keys [first-name
+                                  middle-name
+                                  last-name]
+                   :as patient}]
+               (-> patient
+                   (dissoc :patient/first-name :patient/middle-name :patient/last-name)
+                   (assoc :patient/full-name
+                          (->> [first-name middle-name last-name]
+                               (remove str/blank?)
+                               (str/join " ")))
+                   (update :patient/gender (comp str/capitalize name))
+                   (update :patient/birthday (partial t/format date-formatter)))))
+
+        (some? current-filter)
+        (filter (fn [patient]
+                  (let [attr-values (-> patient
+                                        (dissoc :patient/id)
+                                        vals)]
+                    (some #(str/includes? (str/lower-case %)
+                                          (str/lower-case current-filter))
+                          attr-values))))))))
+
+(rf/reg-sub ::patients-filter
+  (fn [db]
+    (:patients-filter db)))
+
+(defn- search-box []
+  [:input {:style {:width "100%"}
+           :type :text
+           :value (<sub [::patients-filter])
+           :on-change (fn [e]
+                        (>evt [::change-patients-filter (-> e .-target .-value)]))
+           :placeholder "Begin typing your search query here"}])
 
 (def ^:private cell-style
   {:border-bottom "1px solid black"
@@ -54,27 +81,31 @@
 (defn page []
   [:div {:style {:display :flex
                  :justify-content :center}}
-   [:div {:style {:width "1000px"
-                  :display :grid
-                  :grid-template-columns "2fr 1fr 1fr 2fr 150px"}}
-    [:div {:style header-style} "Name"]
-    [:div {:style header-style} "Gender"]
-    [:div {:style header-style} "Birthday"]
-    [:div {:style header-style} "Address"]
-    [:div {:style header-style} "Insurance number"]
-    (doall
-     (for [{:patient/keys [id
-                           full-name
-                           gender
-                           birthday
-                           address
-                           insurance-number]} (<sub [::patients])
-           :let [patient-url (routes/url-for :patient-page {:id id})]]
-       ^{:key id}
-       [:<>
-        [:div {:style cell-style}
-         [:a {:href patient-url} full-name]]
-        [:div {:style cell-style} gender]
-        [:div {:style cell-style} birthday]
-        [:div {:style cell-style} address]
-        [:div {:style cell-style} insurance-number]]))]])
+   [:div {:style {:display :flex
+                  :flex-direction :column
+                  :gap "1rem"}}
+    [:div [search-box]]
+    [:div {:style {:width "1000px"
+                   :display :grid
+                   :grid-template-columns "2fr 1fr 1fr 2fr 150px"}}
+     [:div {:style header-style} "Name"]
+     [:div {:style header-style} "Gender"]
+     [:div {:style header-style} "Birthday"]
+     [:div {:style header-style} "Address"]
+     [:div {:style header-style} "Insurance number"]
+     (doall
+      (for [{:patient/keys [id
+                            full-name
+                            gender
+                            birthday
+                            address
+                            insurance-number]} (<sub [::patients])
+            :let [patient-url (routes/url-for :patient-page {:id id})]]
+        ^{:key id}
+        [:<>
+         [:div {:style cell-style}
+          [:a {:href patient-url} full-name]]
+         [:div {:style cell-style} gender]
+         [:div {:style cell-style} birthday]
+         [:div {:style cell-style} address]
+         [:div {:style cell-style} insurance-number]]))]]])
