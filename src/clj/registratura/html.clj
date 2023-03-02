6e9d98e74c3c8282e5a8cab45cb4766baf9f99fb
Vsevolod Romashov <7@7vn.ru>
2023-03-02 23:08:46 +0400

    change font properties

--- src/clj/registratura/html.clj
+++ src/clj/registratura/html.clj
@@ -8,7 +8,9 @@
     [:title "Registratura"]
     [:style {:type "text/css"}
      "a { text-decoration: none; color: blue; }
-      a:hover { text-decoration: underline; }"]]
+      a:hover { text-decoration: underline; }
+      * { font-family: \"SF Pro Text\",\"SF Pro Icons\",\"Helvetica Neue\",\"Helvetica\",\"Arial\",sans-serif
+          font-size: 11pt; }"]]
    [:body
     [:div#root]
     [:script {:src "/js/main.js"}]]))
